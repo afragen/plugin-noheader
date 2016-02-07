39044cf6f544df503aaf205e0f685a76bfbfc251
Andy Fragen <andy@thefragens.com>
2016-02-07 12:15:59 -0800

    inital

---
+++ plugin-noheader.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Plugin No Header
+ * Plugin URI: https://github.com/afragen/plugin-noheaders
+ * Version: 1.0
+ * Author: Andy Fragen
+ * Description: This is a test plugin for GitHub Updater. It is empty.
+ */

dcbe878312c8bd83fca1f515e52c76114a9e7806
Andy Fragen <andy@thefragens.com>
2016-02-25 08:48:26 -0800

    fix GHU URI

--- plugin-noheader.php
+++ plugin-noheader.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Plugin No Header
- * Plugin URI: https://github.com/afragen/plugin-noheaders
+ * Plugin URI: https://github.com/afragen/plugin-noheader
  * Version: 1.0
  * Author: Andy Fragen
  * Description: This is a test plugin for GitHub Updater. It is empty.
